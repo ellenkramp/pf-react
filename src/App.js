950a533e2cc4f1574a4e225178bd3a7b7e612315
Ellen Kramp <ellenkramp@gmail.com>
2018-06-03 11:28:00 -0400

    updated routes

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ import { Github } from './components/Github';
 import { Experience } from './components/Experience';
 import { Contact } from './components/Contact';
 import { Home } from './components/Home';
+const a = process.env.PUBLIC_URL;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/about" component={About} />
-        <Route path="/education" component={Education} />
-        <Route path="/skills" component={Skills} />
-        <Route path="/github" component={Github} />
-        <Route path="/experience" component={Experience} />
-        <Route path="/contact" component={Contact} />
-        <Route exact path="/" component={Home} />
+        <Route exact path={process.env.PUBLIC_URL + "/about"} component={About} />
+        <Route path={process.env.PUBLIC_URL + "/education"} component={Education} />
+        <Route path={process.env.PUBLIC_URL + "/skills"} component={Skills} />
+        <Route path={process.env.PUBLIC_URL + "/github"} component={Github} />
+        <Route path={process.env.PUBLIC_URL + "/experience"} component={Experience} />
+        <Route path={process.env.PUBLIC_URL + "/contact"} component={Contact} />
+        <Route exact path= {process.env.PUBLIC_URL + '/'} component={Home} />
         <Nav className="nav"/>
       </div>
     )};
